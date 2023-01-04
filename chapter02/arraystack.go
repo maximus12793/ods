0076d91c3926f0b072b1fe565ede8e4f6dfb066f
maximus12793 <maximus1279@gmail.com>
2023-01-04 02:30:54 -0600

    ch2 init

---
+++ chapter02/arraystack.go
@@ -0,0 +1,52 @@
+package ods
+
+import "math"
+
+type ArrayStack struct {
+	a []interface{}
+	n int
+}
+
+func (a *ArrayStack) Size() int {
+	return a.n
+}
+
+func (a *ArrayStack) Init() {
+	a.a = make([]interface{}, 1)
+	a.n = 0
+}
+
+func (a *ArrayStack) Get(i int) interface{} {
+	return a.a[i]
+}
+
+func (a *ArrayStack) Set(i int, x interface{}) interface{} {
+	y := a.a[i]
+	a.a[i] = x
+	return y
+}
+
+func (a *ArrayStack) Add(i int, x interface{}) {
+	if a.n == len(a.a) {
+		a.resize()
+	}
+	copy(a.a[i+1:], a.a[i:a.n])
+	a.a[i] = x
+	a.n++
+}
+
+func (a *ArrayStack) Remove(i int) interface{} {
+	x := a.a[i]
+	copy(a.a[i:a.n-1], a.a[i+1:a.n])
+	a.n--
+	if len(a.a) >= 3*a.n {
+		a.resize()
+	}
+	return x
+}
+
+func (a *ArrayStack) resize() {
+	b := make([]interface{}, int(math.Max(1, float64(2*a.n))))
+	copy(b, a.a)
+	a.a = b
+}
---
+++ chapter02/chapter02_test.go
@@ -0,0 +1,64 @@
+package ods
+
+import "testing"
+
+func TestArrayStack(t *testing.T) {
+	stack := ArrayStack{}
+	stack.Init()
+
+	if stack.Size() != 0 {
+		t.Errorf("stack.Size(): expected %d, got %d", 0, stack.Size())
+	}
+
+	stack.Add(0, "hello")
+	stack.Add(1, true)
+	stack.Add(2, 123)
+
+	if stack.Size() != 3 {
+		t.Errorf("stack.Size(): expected %d, got %d", 3, stack.Size())
+	}
+
+	if v := stack.Get(0); v != "hello" {
+		t.Errorf("stack.Get(0): expected %q, got %v", "hello", v)
+	}
+
+	if v := stack.Get(1); v != true {
+		t.Errorf("stack.Get(1): expected %t, got %v", true, v)
+	}
+
+	if v := stack.Get(2); v != 123 {
+		t.Errorf("stack.Get(2): expected %d, got %v", 123, v)
+	}
+
+	if v := stack.Set(1, false); v != true {
+		t.Errorf("stack.Set(1, false): expected %t, got %v", true, v)
+	}
+
+	if v := stack.Get(1); v != false {
+		t.Errorf("stack.Get(1): expected %t, got %v", false, v)
+	}
+
+	if v := stack.Remove(0); v != "hello" {
+		t.Errorf("stack.Remove(0): expected %q, got %v", "hello", v)
+	}
+
+	if stack.Size() != 2 {
+		t.Errorf("stack.Size(): expected %d, got %d", 2, stack.Size())
+	}
+
+	if v := stack.Remove(1); v != 123 {
+		t.Errorf("stack.Remove(1): expected %d, got %v", 123, v)
+	}
+
+	if stack.Size() != 1 {
+		t.Errorf("stack.Size(): expected %d, got %d", 1, stack.Size())
+	}
+
+	if v := stack.Remove(0); v != false {
+		t.Errorf("stack.Remove(0): expected %t, got %v", false, v)
+	}
+
+	if stack.Size() != 0 {
+		t.Errorf("stack.Size(): expected %d, got %d", 0, stack.Size())
+	}
+}
